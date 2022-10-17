d113c59f88fb89dc72e2b1ae7b4024ef1ca455cb
Maxi <maxiirucci@gmail.com>
2022-10-17 16:02:23 -0300

    new commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import {config} from "dotenv"
 import router from "./routes/routes.js"
-import path, {dirname, join} from 'path'
+import {dirname, join} from 'path'
 import {fileURLToPath} from 'url'
 import morgan from 'morgan'
 import cors from 'cors'
@@ -11,23 +11,13 @@ config()
 const app = express()
 const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(cors()) //modulo para comunicar los dos backeds
-
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 app.use(morgan('dev'))
 app.use(router);
 
-if (process.env.STATE === 'production'){
-    app.use(express.static(join(__dirname,'../client/build'))) //para q pueda ejecutar bien el frontent
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join('../client/build/index.html')) })
-}else{
-    app.get('/',(req,res)=>{
-        res.send('run server')
-    })
-}
 
-
+app.use(express.static(join(__dirname,'../client/build'))) //para q pueda ejecutar bien el frontent
 
 app.set('port', process.env.PORT || 4000);
 
